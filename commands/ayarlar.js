e5cb6f825795401cebd915ef09c4b4032b97521e
Glitch (mahogany-historical-scourge) <none>
2023-12-29 07:55:17 +0000

    Deneme V3

---
+++ commands/ayarlar.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+  name:"ayarlar" ,
+
+  aliases:["settings"],
+
+  code:`
+
+  $author[$username[$clientID]|Ayarlar Sistemi]
+
+  $description[✅|❌ HG BB : **$getGuildVar[hgbb]**
+
+📙|HG BB Kanal :** <#$getGuildVar[hgbbkanal]>**
+
+<a:hosgeldin:1189990168992681994>HG Mesajı : **$getGuildVar[hgmesaj]**
+
+ <a:bb:1190000702165094523>BB Mesajı : **$getGuildVar[bbmesaj]**]
+
+$color[Random]
+
+$footer[$username kullandı.]
+
+  `
+
+}
+
